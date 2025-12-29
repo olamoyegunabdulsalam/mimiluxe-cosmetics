3f856650affab297dea7edb6cb41b4f72429ccee
Olamoyegun Abdulsalam <olamoyegunabdulsalam59@gmail.com>
2025-12-29 10:43:33 +0100

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    copyPublicDir: true,
+  }
 })
